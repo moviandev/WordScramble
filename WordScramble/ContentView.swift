dceb2dad5abc85dce394e039e3389cd680b7808e
Matheus Viana <matheus_o_viana@hotmail.com>
2023-01-25 21:18:47 -0300

    Add sections in list

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -9,9 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        List {            
-            ForEach(0..<5) {
-                Text("Dynamic row \($0)")
+        List {
+            Section("Section 1") {
+                Text("Static row 1")
+                Text("Static row 2")
+            }
+            
+            Section("Section 2") {
+                ForEach(0..<5) {
+                    Text("Dynamic row \($0)")
+                }
+            }
+            
+            Section("Section 3") {
+                Text("Static row 3")
+                Text("Static row 4")
             }
         }
     }
