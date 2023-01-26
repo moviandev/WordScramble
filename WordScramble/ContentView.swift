c834fdf6e778afeede3ad12a0255cbe347e15883
Matheus Viana <matheus_o_viana@hotmail.com>
2023-01-25 21:45:40 -0300

    Rollback

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -12,18 +12,6 @@ struct ContentView: View {
         Text("Hello, world!")
             .padding()
     }
-    
-    func test() {
-        let word = "swift"
-        
-        let checker = UITextChecker()
-        
-        let range = NSRange(location: 0, length: word.utf16.count)
-        
-        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-        
-        let allGood = misspelledRange.location == NSNotFound
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
