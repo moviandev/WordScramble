a3c1aabdaab16d53116a4a144a3dde8ce9ccd711
Matheus Viana <matheus_o_viana@hotmail.com>
2023-01-25 21:40:33 -0300

    creating an array of strings of a string separated by whitespace and we remove all the whitespace of the letter returned to us

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -14,8 +14,15 @@ struct ContentView: View {
     }
     
     func test() {
-        let input = "a b c"
-        let letters = input.components(separatedBy: " ")
+        let input = """
+a
+b
+c
+"""
+        let letters = input.components(separatedBy: "\n")
+        let letter = letters.randomElement()
+        
+        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
     }
 }
 
