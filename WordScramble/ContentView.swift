e29da9f0466cae51a0c3dde37c71d4fd744c5944
Matheus Viana <matheus_o_viana@hotmail.com>
2023-01-25 21:45:22 -0300

    Add UI checker a component from Objective-C

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -14,15 +14,15 @@ struct ContentView: View {
     }
     
     func test() {
-        let input = """
-a
-b
-c
-"""
-        let letters = input.components(separatedBy: "\n")
-        let letter = letters.randomElement()
+        let word = "swift"
         
-        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+        let checker = UITextChecker()
+        
+        let range = NSRange(location: 0, length: word.utf16.count)
+        
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        let allGood = misspelledRange.location == NSNotFound
     }
 }
 
