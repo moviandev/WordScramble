199f64d554b42010feabb1fd3ea71d755a829d62
Matheus Viana <matheus_o_viana@hotmail.com>
2023-01-27 23:39:10 -0300

    Add starting states, usedWords array, rootWord and newWord

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,9 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+
     }
 }
 
